41c655c9bb016597c6a6163fbae30a4ac9360cdc
sonsurim <surim014@naver.com>
2021-09-24 15:28:07 +0900

    chore: 프로젝트 초기 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// eslint + prettier 설정 참고 : https://pravusid.kr/javascript/2019/03/10/eslint-prettier.html
-
 module.exports = {
   env: {
     browser: true,
@@ -10,5 +8,13 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'require-jsdoc': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 }
