b1b047f1fa5aab26b1efd6aa70e75d4d72018ed1
sonsurim <surim014@naver.com>
2021-09-25 01:18:41 +0900

    feat: dom 관련 유틸 함수 구현

---
+++ src/utils/dom.js
@@ -0,0 +1,75 @@
+function $($element) {
+  if (!$element) {
+    return
+  }
+
+  return document.querySelector($element)
+}
+
+function createDom(template) {
+  // const template = '<div>돔파서 테스트</div>';
+
+  const domParser = new DOMParser()
+  const doc = domParser.parseFromString(template, 'text/html')
+  const childNode = Array.from(doc.body.childNodes)
+  const fragment = document.createDocumentFragment()
+
+  fragment.append(...childNode)
+  return fragment
+}
+
+function createElement(element, ...option) {
+  const $element = document.createElement(element)
+
+  if (option.length) {
+    option.forEach((option) => {
+      const optionName = option.substring(1)
+
+      switch (option[0]) {
+        case '#':
+          $element.setAttribute('id', optionName)
+          break
+        default:
+          $element.classList.add(optionName)
+      }
+    })
+  }
+
+  return $element
+}
+
+function addClass($element, ...classNames) {
+  if (!$element) {
+    return
+  }
+
+  classNames.forEach((className) => $element?.classList.add(className))
+}
+
+function addClassAll($NodeList, className) {
+  $NodeList.forEach(($element) => {
+    $element?.classList.add(className)
+  })
+}
+
+function removeClass($element, ...classNames) {
+  if (!$element) {
+    return
+  }
+
+  classNames.forEach((className) => $element?.classList.remove(className))
+}
+
+function removeClassAll($NodeList, className) {
+  $NodeList.forEach(($element) => {
+    $element?.classList.remove(className)
+  })
+}
+
+function replaceClass($element, currentClassName, replaceClassName) {
+  if (!$element) {
+    return
+  }
+
+  $element.classList.replace(currentClassName, replaceClassName)
+}
