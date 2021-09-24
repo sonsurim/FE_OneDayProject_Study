7ebe8d6581bf1815ef3ec6b52737f8cb47e63928
Seungrok Yoon <sonicrok93@gmail.com>
2021-09-24 18:26:10 +0900

    feat: connected PeopleInfo component to App.js and added StyleSheet.css

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-export default function App({ target }) {}
+import PeopleInfo from './PeopleInfo.js'
+
+export default function App({ target }) {
+  this.state = {
+    currentPeople: '',
+  }
+
+  this.setState = (nextState) => {
+    this.state = nextState
+  }
+
+  new PeopleInfo({
+    target,
+    initialState: this.state,
+    onSubmit: (text) => {
+      console.log(text)
+      return 0
+    },
+  })
+}
--- src/PeopleInfo.js
+++ src/PeopleInfo.js
@@ -1,3 +1,27 @@
-export default function PeopleInfo() {
-  return 1
+export default function PeopleInfo({ target, initialState, onSubmit }) {
+  // [{이름: “윤승록”, MBTI: “INFP”}, …]
+  const infoFormat = document.createElement('div')
+  infoFormat.className = 'informationForm' // css적용을 위한 클래스이
+
+  target.appendChild(infoFormat)
+
+  this.render = () => {
+    infoFormat.innerHTML = `
+    <div class= 'peopleInfoBlock'>
+      <textarea class='inputText'  placeholder='사용자 정보를 입력해 주세요' /></textarea>
+      <button class='submitbutton'>팀 제작 시작</button>
+    <div>
+    `
+  }
+
+  this.render()
+
+  infoFormat.addEventListener('click', (e) => {
+    const target = e.target.closest('button')
+    if (target) {
+      const textareaElement = e.target.parentNode.childNodes[1]
+      const text = textareaElement.value
+      onSubmit(text)
+    }
+  })
 }
